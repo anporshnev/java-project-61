daa1abb609eb2b51ea66aaf8a9c7865ec1d48309
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-01-18 12:47:47 +0300

    Добавляет игру Простое ли число?

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,35 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class Prime {
+    public static void startGame() {
+        createTasks();
+        Engine.greet();
+        Engine.showTaskDescription("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+        Engine.startTask();
+    }
+
+    private static void createTasks() {
+        for (var i = 0; i < Engine.REQUIRED_NUMBER_ATTEMPTS; i++) {
+            var randomNumber = Engine.random.nextInt(100) + 1;
+            var trueAnswer = isPrime(randomNumber) ? "yes" : "no";
+            Engine.questions[i] = Integer.toString(randomNumber);
+            Engine.trueAnswers[i] = trueAnswer;
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+        var startNumber = 2;
+        while (startNumber < number) {
+            if (number % startNumber == 0) {
+                return false;
+            }
+            startNumber++;
+        }
+        return true;
+    }
+}
