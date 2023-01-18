b3dda63d3e287d71efcc922428297fca173c73c9
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-01-18 12:45:13 +0300

    Добавляет движок для игр, основное меню выбора игр

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
+import hexlet.code.games.GCD;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Even;
+
+
+
+import java.util.Scanner;
+
 public class App {
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[] menuItems
+                = {"1 - Greet", "2 - Even", "3 - Calc", "4 - GCD", "5 - Progression", "6 - Prime", "0 - Exit"};
+
+        System.out.println("Please enter the game number and press Enter.");
+        for (var item: menuItems) {
+            System.out.println(item);
+        }
+        System.out.print("Your choice: ");
+        String choice = scanner.next();
+
+        switch (choice) {
+            case "1" -> Engine.greet();
+            case "2" -> Even.startGame();
+            case "3" -> Calc.startGame();
+            case "4" -> GCD.startGame();
+            case "5" -> Progression.startGame();
+            case "6" -> Prime.startGame();
+            case "0" -> scanner.close();
+            default -> System.out.println("There is no game under this number!");
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,47 @@
+package hexlet.code;
+
+import java.util.Random;
+
+public class Engine {
+    public static Random random = new Random();
+    public static String gamerName;
+    public static final int REQUIRED_NUMBER_ATTEMPTS = 3;
+    public static int numberAttempts = 0;
+    public static String[] questions = new String[REQUIRED_NUMBER_ATTEMPTS];
+    public static String[] trueAnswers = new String[REQUIRED_NUMBER_ATTEMPTS];
+
+    public static void greet() {
+        System.out.println();
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        gamerName = App.scanner.next();
+        System.out.println("Hello, " + gamerName + "!");
+    }
+
+    public static void showTaskDescription(String text) {
+        System.out.println(text);
+    }
+
+    public static void startTask() {
+        for (var i = 0; i < Engine.REQUIRED_NUMBER_ATTEMPTS; i++) {
+            System.out.println("Question: " + questions[i]);
+            System.out.print("Your answer: ");
+            var answer = App.scanner.next();
+
+            if (answer.equals(trueAnswers[i])) {
+                System.out.println("Correct!");
+            } else {
+                var message = """
+                        '%s' is wrong answer ;(. Correct answer was '%s'.
+                        Let's try again, %s!
+                        """.formatted(answer, trueAnswers[i], gamerName);
+                System.out.print(message);
+                break;
+            }
+            numberAttempts++;
+        }
+        if (numberAttempts == REQUIRED_NUMBER_ATTEMPTS) {
+            System.out.println("Congratulations, " + gamerName + "!");
+        }
+    }
+}
